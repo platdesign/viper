9169ecaa1ad6d66575f09e0442b181fa2f33ff49
Christian Blaschke <mail@platdesign.de>
2015-02-18 21:28:54 +0100

    refactored

--- example/index.js
+++ example/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var Q = require('q');
+var Sequelize = require('sequelize');
 
 var viper = require('../');
 
@@ -34,10 +36,64 @@ var app = viper({
 	}
 });
 
-
+/*
 app.plugin( require('./plugins/session-sequelize.js') );
 app.plugin( require('../../plugins/viper-plugin-sequelize') );
 app.plugin( require('../../plugins/viper-plugin-jsonapi') );
 app.plugin( require('../../plugins/viper-plugin-jade-mvc') );
 app.plugin( require('../../plugins/viper-plugin-account-sequelize') );
+*/
+
+
+app.config(function($routeProvider, $serverProvider, $dbProvider) {
+
+
+	$dbProvider.connect('db', function() {
+
+	});
+
+	$serverProvider
+		.registerSessionStore(function(sessionMiddleware, $db) {
+			var SequelizeStore = require('connect-sequelize')(sessionMiddleware);
+			return new SequelizeStore($db.db);
+		});
+
+	$routeProvider
+		.route({
+			url: '/',
+			//method: 'GET',
+			//template: './views/test.jade',
+			resolve: {
+				test: function() {
+					return 123;
+				}
+			},
+			controller: function($scope, $account, test) {
+				$scope.test = test;
+				$scope.account = $account;
+			},
+			permissions: {
+				only: ['authenticated']
+			}
+
+		})
+		.route({
+			url: '/test/:name',
+			//method: 'GET',
+			//template: './views/test.jade',
+		})
+	;
+
+});
+
+app.run(function($db, $log) {
+
+});
+
+
+
+app.bootstrap();
+
+
+
 
