8ac721b4abaebbf0c68ee681867b44fc11f4541c
Christian Blaschke <mail@platdesign.de>
2015-02-26 16:01:01 +0100

    feature: subroutes in route definition by 'routes' attribute

--- example/index.js
+++ example/index.js
@@ -21,40 +21,42 @@ app.config(function($routeProvider, $q, $serverProvider) {
 				_firstResolver: function() {
 					return 'First Resolver';
 				}
-			}
-		})
-		.route({
-			url: '/:test',
-			resolve: {
-				_secondResolver:function(_firstResolver, $req, $account){
-					return {
-						_firstResolver: _firstResolver,
-						_secondResolver: $req.params.test,
-						$account: $account
-					};
-				},
-				_thirdResolver:function(_secondResolver) {
-					_secondResolver._thirdResolver = '123';
-					return _secondResolver;
-				}
 			},
-			controller: function(_thirdResolver) {
-				return _thirdResolver
-			}
-		})
-		.route({
-			url: '/:test/a',
-			resolve: {
-				_finally: function (_thirdResolver) {
-					console.log('called _finally');
-					return _thirdResolver;
+			routes: [
+				{
+					url: '/:test',
+					resolve: {
+						_secondResolver:function(_firstResolver, $req, $account){
+							return {
+								_firstResolver: _firstResolver,
+								_secondResolver: $req.params.test,
+								$account: $account
+							};
+						},
+						_thirdResolver:function(_secondResolver) {
+							_secondResolver._thirdResolver = '123';
+							return _secondResolver;
+						}
+					},
+					controller: function(_thirdResolver) {
+						return _thirdResolver;
+					},
+					routes: [{
+						url: '/a',
+						resolve: {
+							_finally: function (_thirdResolver) {
+								console.log('called _finally');
+								return _thirdResolver;
+							}
+						},
+						controller: function(_finally, $req) {
+							console.log('called controller');
+							//return _finally
+							return _finally;
+						}
+					}]
 				}
-			},
-			controller: function(_finally, $req) {
-				console.log('called controller');
-				//return _finally
-				return _finally;
-			}
+			]
 		})
 	;
 
